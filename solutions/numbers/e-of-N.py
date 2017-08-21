9cfc272ddf662a9ce786704810797151f3c9adf0
Prithvi Raju <nprithviraj24@gmail.com>
2017-08-21 19:34:05 +0530

    E of number done; Working only in Python 2.x version.

---
+++ solutions/numbers/e-of-N.py
@@ -0,0 +1,10 @@
+# exponential of a number.
+
+import math  
+
+number = input("Enter the number: ")
+type(number)
+
+#print("Value: ")
+#print("Value" +str(math.exp(number)))
+print("Exponential of the number is: "+ str(math.exp(number)))
