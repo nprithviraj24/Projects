13dc84cd0a0ea2e1a5e82c56b85632b58bb94a71
Prithvi Raju <nprithviraj24@gmail.com>
2017-09-01 01:35:00 +0530

    GUI Programming using Tkinter library

---
+++ solutions/numbers/mortgage-calc.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+
+counter = 0
+def counter_label(label):
+    counter = 0
+    def count():
+        global counter
+        counter +=1
+        label.config(text=str(counter))
+        label.after(1000, count)
+    count()
+
+
+
+root = tk.Tk()
+root.title("Counting")
+
+
+
